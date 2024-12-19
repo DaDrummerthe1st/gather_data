28cdacdba6de3dadd974931d290c233bdb58c4f6
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2024-12-19 14:16:55 +0100

    started logging through textfile

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from datetime import datetime
 import pytz
+import logging
 
 categories = 'https://api.chucknorris.io/jokes/categories'
 now_time = datetime.now(pytz.timezone('Europe/Stockholm'))
+
+logging.basicConfig(
+    filename=""
+)
 
 
 class retrieve_chuck_norris_jokes:
@@ -18,9 +23,12 @@ class retrieve_chuck_norris_jokes:
         except Exception as e:
             print('unknonw error: ' + str(e))
         else:
-            if response.status_code == 200:
-                data = response.json()
-                print(data)
+            try:
+                if response.status_code == 200:
+                    categories = response.json()
+            except Exception as e:
+                print("unknown error occured while retrieving " + str(e))
+                logging.                
 
 
 
