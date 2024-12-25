97f22f8c8bc521230259b092b98ca0d123791c29
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2024-12-25 04:16:27 +0100

    Lots of changes to focus project

--- mylibraries/retrievals/chuck_norris.py
+++ mylibraries/retrievals/chuck_norris.py
@@ -1,11 +1,12 @@
 import requests
 import logging
 import pandas as pd
+import sqlite3
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    filename='resources/general.log',
+    filename='../../resources/general.log',
     level=logging.DEBUG,
     format='[%(asctime)s][%(levelname)s][%(filename)s][%(lineno)d] %(message)s'
 )
@@ -13,23 +14,54 @@ logging.basicConfig(
 
 class RetrieveChuckNorrisJokes:
     """Since there might be other sources to retrieve from"""
-    def __init__(self):
+    def __init__(self, database_file):
         # self.categories_url = ""
-        self.chuck_norris_jokes = {
-            "id": [],
-            "created_at": [],
-            "updated_at": [],
-            "value": [],
-            "category": []
-        }
-        self.pd = pd.DataFrame(self.chuck_norris_jokes)
+        # self.chuck_norris_jokes = {
+        #     "id": [],
+        #     "created_at": [],
+        #     "updated_at": [],
+        #     "value": [],
+        #     "category": []
+        # }
+
+        self.retrieved_df = ""
+
+        self.retrieved_joke_id = ""
+        self.retrieved_joke_created_at = ""
+        self.retrieved_joke_updated_at = ""
+        self.retrieved_joke_url = ""
+        self.retrieved_joke_value = ""
+        self.retrieved_joke_category = ""
+
+        self.does_exist_or_not = ""
+
+        # Database connection
+        self.database_file = database_file
+        self.connection_database = sqlite3.connect(self.database_file)
+        try:
+            self.connection_cursor = self.connection_database.cursor()
+        except FileNotFoundError as e:
+            print("FileNotFoundError: " + str(e))
+        except sqlite3.OperationalError as e:
+            print("sqlite3.OperationalError: " + str(e))
+        else:
+            print("connection cursor" + str(type(self.connection_cursor)))
+
+    def check_for_existance(self):
+        """This method does the following:
+        - checks connection
+        - searches database for retrieved joke id
+        purpose is to return boolean regarding uniique ids
+        """
+        comaparison = self.connection_cursor.execute("select id from jokes where id=?", (self.retrieved_joke_id,)).fetchall()
+        if not comaparison:
+
+        print ("here it is")
+       #(Tkw1X2aESzCyrjqeSTG-RA', '2020-01-05 13:42:25.905626', '2020-01-05 13:42:25.905626', 'https://api.chucknorris.io/jokes/Tkw1X2aESzCyrjqeSTG-RA', 'Chuck Norris has a new policy regarding gays in the military. Dont ask - EVER!')""")
+        # self.connection_cursor.execute('insert into jokes values (?, ?, ?, ?)', (Tkw1X2aESzCyrjqeSTG-RA', '2020-01-05 13:42:25.905626', '2020-01-05 13:42:25.905626', 'https://api.chucknorris.io/jokes/Tkw1X2aESzCyrjqeSTG-RA', 'Chuck Norris has a new policy regarding gays in the military. Dont ask - EVER!')""")
 
     def retrieve_new_joke(self):
-        """
-        By ranomdly fetching a joke and then testing it,
-        fills up the database with new Chuck Norris jokes
-        """
-        print(self.chuck_norris_jokes)
+        """Fetching a random joke"""
         try:
             response = requests.get(
                 'https://api.chucknorris.io/jokes/random', timeout=10)
@@ -41,21 +73,21 @@ class RetrieveChuckNorrisJokes:
             print("Connection Error: %s", e)
             logger.error("Connection Error: %s", e)
         else:
-            # print(self.pd["id"])
-            print(response_json)
-            try:
-                response_df = pd.DataFrame(response_json)
-                print(response_df + "is the answer")
-            except ValueError as val_err:
-                print("ValueError: " + str(val_err))
-            else:
-                for joke_id in self.pd["id"]:
-                    # print(joke_id)
-                    # print(response_df)
-                    if response_df["id"] != joke_id:
-                        print("this joke is not found in the database")
-                        # self.pd["id"] = self.pd[id]
-                        # self.pd[""]
+            self.retrieved_df = pd.DataFrame(response_json)
+            self.retrieved_joke_id = response_json['id']
+            print(self.retrieved_joke_id)
+            self.retrieved_joke_created_at = response_json['created_at']
+            self.retrieved_joke_updated_at = response_json['updated_at']
+            self.retrieved_joke_url = response_json['url']
+            self.retrieved_joke_value = response_json['value']
+            #self.retrieved_joke_category = response_json['category']
+
+    def add_joke_to_database(self):
+        pass
+
+    def close_all_connections(self):
+        self.connection_cursor.close()
+        self.connection_database.close()
 
     # def retrieve_categories(self, categories_url) -> list:
     #     """UNUSED
@@ -117,8 +149,13 @@ class RetrieveChuckNorrisJokes:
     #             print(self.chuck_norris_jokes)
 
 
-categories = 'https://api.chucknorris.io/jokes/categories'
+# categories = 'https://api.chucknorris.io/jokes/categories'
+actual_path = "../../resources/database/chuck_norris_jokes.db"
 
-chuck_norris1 = RetrieveChuckNorrisJokes
+
+chuck_norris1 = RetrieveChuckNorrisJokes(database_file=actual_path)
 #chuck_norris1.retrieve_categories(categories_url=categories)
 chuck_norris1.retrieve_new_joke()
+chuck_norris1.check_for_existance()
+
+chuck_norris1.close_all_connections()
